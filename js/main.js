2cb352df20d5f173263e13b32898e60e842beb2d
Hampus Avekvist <hampus@avekvist.com>
2019-03-30 11:14:28 +0100

    Undid the animation in JS

--- js/main.js
+++ js/main.js
@@ -1,42 +1,21 @@
-var baseNavPadding;
-var navHeight;
-
 $(document).ready(function() {
-    baseNavPadding = parseInt($("#inner-nav a").css("padding-top")) * 2;
     var initialize = function() {
-        navHeight = $("#handle").height() * 4 + baseNavPadding;
-
         if($(document).width() <= 960) {
-            if($("#inner-nav").css("display") != "none") {
-                $("#inner-nav").css("display", "none");
-                $("#inner-nav").css("height", 0);
-            }
+            if($("#inner-nav").css("display") != "none")
+                $("#inner-nav").toggle();
         } else {
             if($("#inner-nav").css("display") == "none")
-                $("#inner-nav").css("display", "grid");
+                $("#inner-nav").toggle();
         }
     }
 
     initialize();
 
     $("#handle").on("click", function() {
-        if($("#inner-nav").css("display") == "none") {
-            $("#inner-nav").css("display", "grid");
-            $("#inner-nav").animate({
-                height: navHeight,
-            }, 500);
-        } else {
-            $("#inner-nav").animate({
-                height: 0,
-            }, 500, function() {
-                $("#inner-nav").css("display", "none");
-            });
-        }
+        $("#inner-nav").toggle();
     });
 
     $(window).resize(function() {
-        baseNavPadding = parseInt($("#inner-nav a").css("padding-top")) * 2;
-        console.log(baseNavPadding);
         initialize();
     });
 });
